b04ede755f31234dc6635bb0b6d0130b7cbc6fc7
hirorocky <hirorocky@gmail.com>
2023-03-25 22:19:03 +0900

    fix: 環境変数名が間違っていた - ついでに更新頻度も低めに

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.SITE_URL,
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
+  changefreq: "weekly",
   outDir: "./out",
   generateRobotsTxt: true,
 };

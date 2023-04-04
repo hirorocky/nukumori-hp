8aee3dfead4d5cd4e94e09e4477b867dee621f98
hirorocky <hirorocky@gmail.com>
2023-04-04 21:20:53 +0900

    fix: アイコンが細かったので

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home()
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a href="https://mi.nukumori-gay.space" className="inline-flex rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">
-                  <svg className="stroke-1 hover:stroke-2 w-5 h-5 mr-2" aria-hidden="true" stroke="currentColor" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <svg className="stroke-2 w-5 h-5 mr-2" aria-hidden="true" stroke="currentColor" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
                   </svg>
                   覗いてみる
